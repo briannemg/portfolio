2d6e6720826a94ad6712394c43cde13467ce791e
Brianne Nelson <briannemg@icloud.com>
2026-06-23 08:31:48 -0500

    refactor: simplify portfolio architecture to single-page React app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Resume from "./pages/Resume";
-import Contact from "./pages/Contact";
-import Layout from "./components/Layout";
-
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="resume" element={<Resume />} />
-        <Route path="contact" element={<Contact />} />
-        {/* Future routes like projects can go here too */}
-      </Route>
-    </Routes>
+    <main>
+      <h1>Brianne Nelson</h1>
+      <p>Forecast verification • data analysis • Python automation</p>
+    </main>
   );
 };
 
